1a3c1e35663c2d328f00a3ad319f59cd81c35bb4
Morshed <golammorshed004@gmail.com>
2026-05-18 18:09:42 +0600

    Implement lipgloss

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	tea "charm.land/bubbletea/v2"
+	"charm.land/lipgloss/v2"
 )
 
 type model struct {
@@ -11,12 +13,27 @@ type model struct {
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+
+	// Is it a key press?
+	case tea.KeyPressMsg:
+
+		// Cool, what was the actual key pressed?
+		switch msg.String() {
+
+		// These keys should exit the program.
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+		}
+	}
+
 	return m, nil
 }
 
 func InitializeModel() model {
 	return model{
-		message: "🙂 Welcome to Termnote: ",
+		message: "Welcome to Termnote: ",
 	}
 }
 
@@ -25,13 +42,20 @@ func (m model) Init() tea.Cmd {
 }
 
 func (m model) View() tea.View {
-	return tea.NewView(m.message)
+	var style = lipgloss.NewStyle().
+		Bold(true).
+		Foreground(lipgloss.Color("16")).
+		Background(lipgloss.Color("46")).
+		PaddingLeft(4).
+		PaddingRight(4)
+	help:= "Ctrl + C: Quit, \nCtrl + N: New File, \nCtrl + L: List, \nCtrl + S: Save, \nEsc: Back/Save\n"
+	return tea.NewView(fmt.Sprintf("\n%s\n\n%s\n", style.Render(m.message), help))
 }
 
 func main() {
 	p := tea.NewProgram(InitializeModel())
 
-	if _, err:=p.Run(); err != nil{
+	if _, err := p.Run(); err != nil {
 		log.Fatal(err)
 	}
 }
