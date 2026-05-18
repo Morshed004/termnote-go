1a172203c471867163bfc1e9c0fab52dd14ea3fc
Morshed <golammorshed004@gmail.com>
2026-05-18 17:49:43 +0600

    Setup bubbletea

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	tea "charm.land/bubbletea/v2"
+)
+
+type model struct {
+	message string
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	return m, nil
+}
+
+func InitializeModel() model {
+	return model{
+		message: "🙂 Welcome to Termnote: ",
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) View() tea.View {
+	return tea.NewView(m.message)
+}
 
 func main() {
-	fmt.Println("Welcome to Termnote.")
+	p := tea.NewProgram(InitializeModel())
+
+	if _, err:=p.Run(); err != nil{
+		log.Fatal(err)
+	}
 }
